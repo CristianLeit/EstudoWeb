22f8bbe48bc7bac2f8eaf118f31825db5d5b7bbd
cristianleit <cristianleite.silva@mackenzista.com.br>
2024-10-07 19:52:41 -0300

    dia 7 - termino da seção 6

--- funcao/IIFE.js
+++ funcao/IIFE.js
@@ -1,6 +1,16 @@
 //IIFE -> Immediately Invoked Function Expression
 
-(function(){
-    console.log('Será executado na hora!');
-    console.log('Foge do escopo mais abragente!');
+/*const valor = 110
+const porcent = 0.3 // não alcança essa função, porque é escopo local*/
+
+//função auto invocada
+(function () {
+    const valor = 110
+    const porcent = 30
+    const valorDoPorcent = porcent/100
+
+    console.log(`30% de 110 é ${valor * valorDoPorcent}`);
+    console.log(`acréscimo de 30% no 110 é ${valor * valorDoPorcent + valor}`);
+    console.log(`decréscimo de 30% no 110 é ${valor - valor * valorDoPorcent}`);
+    console.log('Foge do escopo mais abragente é invocação imediata!');
 })()
