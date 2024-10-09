809462379789cfba35b2c3c92883cd183ce9b6cd
cristianleit <cristianleite.silva@mackenzista.com.br>
2024-10-09 00:31:16 -0300

    exe

--- Exercícios1/exe6.js
+++ Exercícios1/exe6.js
@@ -1,17 +1,18 @@
 const juros = (ju) => 1+(ju/100)
 function simples (c, j, t ){ 
-    const montante = c*((1+j/100)*(t/12))
+    const montante = c+(c*(j/100)*t)
     console.log(`
         Simples
-    capital inicial: ${c} 
+    capital inicial: ${c} juros: ${j}%
     montante: ${montante}`)
 }
 function composto (c, j, t ){ 
-    const montante = c * Math.pow((juros(j)),t)
+    const montante = c * Math.pow(juros(j),t)
     console.log(`
         Composto
-    capital inicial: ${c} 
+    capital inicial: ${c} juros: ${j}%
     montante: ${montante}`)
 }
 simples(100, 50, 10 )
+simples(100000, 10, 4)
 composto(1000, 2, 2)
---
+++ Exercícios2/fundamentos06.js
@@ -0,0 +1,12 @@
+function jurosSimples (capitalInicial, taxa, tempo) {
+    return capitalInicial + (capitalInicial * taxa * tempo)
+}
+
+function jurosCompostos (capitalInicial, taxa, tempo) {
+    return capitalInicial * (1 + taxa) ** tempo
+}
+
+console.log(jurosSimples(100, 10/100, 2));
+console.log(jurosSimples(10000, 10/100, 4));
+console.log(jurosCompostos(1000, 2/100, 2));
+
