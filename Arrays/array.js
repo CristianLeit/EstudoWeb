9bcac68c2d36792d4ed2fa5508f446c47c976fe7
cristianleit <cristianleite.silva@mackenzista.com.br>
2024-10-26 23:38:35 -0300

    dia 15 - array

--- Arrays/array.js
+++ Arrays/array.js
@@ -9,23 +9,23 @@ console.log(aprovados[1]);
 console.log(aprovados[2]);
 console.log(aprovados[3]);
 
-aprovados[3]= 'Paulo'
+aprovados[3] = 'Paulo'
 aprovados.push('Abia')
 console.log(aprovados.length);
 
 aprovados[9] = 'Rafael'
 console.log(aprovados.length);
-console.log(aprovados[8]===undefined);
+console.log(aprovados[8] === undefined);
 console.log(aprovados)
 
 aprovados.sort()
 console.log(aprovados);
 
- delete aprovados[1]
- console.log(aprovados[1]);
- console.log(aprovados[2]);
+delete aprovados[1]
+console.log(aprovados[1]);
+console.log(aprovados[2]);
 
- aprovados= ['Bia','Carlos', 'Ana']
- aprovados.splice(1,1)
+aprovados = ['Bia', 'Carlos', 'Ana']
+aprovados.splice(1, 1)
 
- console.log(aprovados);
+console.log(aprovados);
