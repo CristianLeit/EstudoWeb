c9e4bef6d0f8552ebcbab542b6ce31ad97c5f7de
cristianleit <cristianleite.silva@mackenzista.com.br>
2024-11-09 23:13:40 -0300

    18 maioridade

--- Arrays/reduce3.js
+++ Arrays/reduce3.js
@@ -2,7 +2,7 @@ Array.prototype.reduce2 = function (callback, valorInicial) {
     const indiceInicial = valorInicial ? 0 : 1
     let acumulador = valorInicial || this[0]
     for (let i = indiceInicial; i < this.length; i++) {
-        acumulador = callback(acumulador, this[i], i, this)        
+        acumulador = callback(acumulador, this[i], i, this)
     }
     return acumulador
 }
