2dfd9952952550fa22ec1815bfc4467fca1faaf6
cristianleit <cristianleite.silva@mackenzista.com.br>
2024-12-09 00:00:03 -0300

    dia 29 - promise

--- esnext/promise_5.js
+++ esnext/promise_5.js
@@ -1,26 +1,26 @@
 const { rejects } = require("assert");
 const { resolve } = require("path");
 
-function funcionarOuNao(valor, chanceErro){
+function funcionarOuNao(valor, chanceErro) {
     return new Promise((resolve, rejects) => {
-        try{
+        try {
             con.log('temp')
-            if(Math.random()< chanceErro){
+            if (Math.random() < chanceErro) {
                 rejects('Ocorreu um erro!')
-            }else{
+            } else {
                 resolve(valor)
             }
-        } catch(e){
+        } catch (e) {
             rejects(e)
         }
     })
 }
 funcionarOuNao('Testando...', 0.9)
-    .then(v=>console.log(`Valor: ${v}`))
+    .then(v => console.log(`Valor: ${v}`))
     .then(
         v => consolo.log(v),
         err => console.log(`Erro Esp.: ${err}`)
-        )
-    .then(()=> console.log('Quase Fim!'))
+    )
+    .then(() => console.log('Quase Fim!'))
     .catch(err => console.log(`Erro: ${err}`))
-    .then(()=> console.log('Fim!'))
+    .then(() => console.log('Fim!'))
