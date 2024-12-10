2b70cee81aebd0b2542f29ba187e9495fe2cfaf8
cristianleit <cristianleite.silva@mackenzista.com.br>
2024-12-09 23:55:51 -0300

    dia 30

--- esnext/async_await1.js
+++ esnext/async_await1.js
@@ -1,8 +1,8 @@
 const { resolve } = require("path")
 
-function esperarPor(tempo = 2000){
-    return new Promise(function(resolve){
-        setTimeout(() =>resolve(),tempo)
+function esperarPor(tempo = 2000) {
+    return new Promise(function (resolve) {
+        setTimeout(() => resolve(), tempo)
     })
 }
 
@@ -13,30 +13,30 @@ function esperarPor(tempo = 2000){
     .then(esperarPor)
     .then(()=> console.log('Executando promise 3...'))
  */
-function retornarValor(){
-    return new Promise(resolve =>{
-        setTimeout(()=> resolve(10), 5000)
+function retornarValor() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(10), 5000)
     })
 }
-async function retornarValorRapido(){
+async function retornarValorRapido() {
     return 20
 }
 
-async function executar(){
+async function executar() {
     let valor = await retornarValorRapido()
     await esperarPor(1500)
     console.log(`Async/Await ${valor}...`)
 
     await esperarPor(1500)
-    console.log(`Async/Await ${valor+1}...`)
+    console.log(`Async/Await ${valor + 1}...`)
 
     await esperarPor(1500)
-    console.log(`Async/Await ${valor+2}...`)
+    console.log(`Async/Await ${valor + 2}...`)
 
     return valor + 3
 }
 
-async function executarDeVerdade(){
+async function executarDeVerdade() {
     const valor = await executar()
     console.log(valor)
 }
