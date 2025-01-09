ccd1d2358bf497aadffb116f316ac3f0faa9c38a
cristianleit <cristianleite.silva@mackenzista.com.br>
2025-01-08 22:02:28 -0300

    seletores css

--- css/js/tag.js
+++ css/js/tag.js
@@ -12,17 +12,17 @@ const colors = {
     form: '#9f6581',
     body: '#25b6da',
     padrao: '#616161',
-    get(tag){
-        return this[tag]? this[tag]: this.padrao
+    get(tag) {
+        return this[tag] ? this[tag] : this.padrao
     }
 }
 
 document.querySelectorAll('.tag').forEach(elemento => {
     const tagName = elemento.tagName.toLowerCase()
-    
+
     elemento.style.borderColor = colors.get(tagName)
 
-    if (!elemento.classList.contains('nolabel')){
+    if (!elemento.classList.contains('nolabel')) {
         const label = document.createElement('label')
         label.style.backgroundColor = colors.get(tagName)
         label.innerHTML = tagName
